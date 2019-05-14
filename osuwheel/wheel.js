419ddd4ef7a02508cd12879e2c9734fa3a281320
mimilasyxa <50004735+mimilasyxa@users.noreply.github.com>
2019-05-14 17:36:38 +0700

    new

---
+++ osuwheel/wheel.js
@@ -0,0 +1,31 @@
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext('2d');
+let dX = 1,
+colours = [ "black", "white" , "green" , "yellow"];
+
+let gH = canvas.height = window.innerHeight;
+    gW =canvas.width = window.innerWidth;
+
+window.requestAnimationFrame(draw);
+function draw() {
+ctx.clearRect(0,0,10000,10000);
+triangle();
+ctx.beginPath();
+sectors();
+ctx.stroke();
+window.requestAnimationFrame(draw);
+}
+
+function triangle(){
+    ctx.beginPath();
+    ctx.moveTo(gW/2 - 410, gH/2);
+    ctx.lineTo(gW/2 - 450, gH/2 -20);
+    ctx.lineTo(gW/2 - 450, gH/2 +20);
+    ctx.fill();
+}
+
+function sectors() {
+    ctx.lineTo(gW/2,gH/2);
+    ctx.arc(gW/2,gH/2,400,0,Math.PI/13);
+    ctx.lineTo(gW/2,gH/2);
+}
