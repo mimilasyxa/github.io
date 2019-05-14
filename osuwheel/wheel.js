4c7fdcd356b930392f5575b12115b2033b8b899c
mimilasyxa <50004735+mimilasyxa@users.noreply.github.com>
2019-05-14 21:56:00 +0700

    important

--- osuwheel/wheel.js
+++ osuwheel/wheel.js
@@ -1,11 +1,16 @@
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext('2d');
-let dX = 1,
+let radius=400,
+    triToCir=400;
 colours = [ "black", "white" , "green" , "yellow"];
 
 let gH = canvas.height = window.innerHeight;
     gW =canvas.width = window.innerWidth;
 
+if (gH < 1000 && gW < 1400){
+    raduis=200;
+    triToCir=200;
+}
 
 window.requestAnimationFrame(draw);
 function draw() {
@@ -15,20 +20,22 @@ results();
 setTimeout(sectors(),1000);
 ctx.stroke();
 triangle();
+ctx.restore();
 window.requestAnimationFrame(draw);
 }
 
 function triangle(){
+    ctx.save();
     ctx.beginPath();
-    ctx.moveTo(gW/2 - 410, gH/2);
-    ctx.lineTo(gW/2 - 450, gH/2 -20);
-    ctx.lineTo(gW/2 - 450, gH/2 +20);
+    ctx.moveTo(gW/2 - triToCir - 10, gH/2);
+    ctx.lineTo(gW/2 - triToCir - 50, gH/2 -20);
+    ctx.lineTo(gW/2 - triToCir - 50, gH/2 +20);
     ctx.fill();
 }
 
 function sectors() {
     for (let i=0 ; i<=2 ; i+=0.20){
-        ctx.arc(gW/2,gH/2,400,0,Math.PI*i,false);   
+        ctx.arc(gW/2,gH/2,radius,0,Math.PI*i,false);   
         ctx.lineTo(gW/2,gH/2);
     }
     ctx.translate(gW/2,gH/2);
