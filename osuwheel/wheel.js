880bf97908d17b55a0b166e1ef95a63e0703858a
mimilasyxa <50004735+mimilasyxa@users.noreply.github.com>
2019-05-15 19:40:12 +0700

    Update wheel.js

--- osuwheel/wheel.js
+++ osuwheel/wheel.js
@@ -10,10 +10,10 @@ canvas.width = document.documentElement.clientWidth;
 
 let gH = canvas.height,
     gW = canvas.width;
-    scaleH = stdHeight - gH;
+    scaleH = gH / stdHeight;
 
-triToCir=400 - scaleH;
-CircRad = 400 - scaleH;
+triToCir=400 * scaleH;
+CircRad = 400 * scaleH;
 
 window.requestAnimationFrame(draw);
 function draw() {
@@ -29,8 +29,8 @@ window.requestAnimationFrame(draw);
 function triangle(){
     ctx.beginPath();
     ctx.moveTo(gW/2 - triToCir - 10, gH/2);
-    ctx.lineTo(gW/2 - triToCir - 50, gH/2 -20);
-    ctx.lineTo(gW/2 - triToCir - 50, gH/2 +20);
+    ctx.lineTo(gW/2 - triToCir - 50, gH/2 + (-20 * scaleH));
+    ctx.lineTo(gW/2 - triToCir - 50, gH/2 +(20 * scaleH));
     ctx.fill();
 }
 
@@ -45,6 +45,6 @@ function sectors() {
 }
 
 function results(){
-    ctx.font = "24px serif";
+    ctx.font = 24*scaleH +"px serif";
     ctx.strokeText("Pass 7 star map w/o NF", gW/2+50, gH/2-10);
 }
