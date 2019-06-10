fa146049e02a74c95d8fabf321c93d75d53f8745
mimilasyxa <50004735+mimilasyxa@users.noreply.github.com>
2019-06-10 20:55:53 +0700

    Update script.js

--- cardGame/script.js
+++ cardGame/script.js
@@ -2,36 +2,60 @@ let divs = document.querySelectorAll(".item"),
     cards = Array.from(divs),
     parentElement = document.querySelector(".container"),
     cardObj = [],
-    colours = ["red","green","pink",'yellow'];
+    colours = ["red","green","pink",'yellow',"blue","orange"],
+    cardClicked = false,
+    first,second;
 
 class Card {
     constructor(color){
         this.color = color;
+        this.flipped = false;
     }
     add(){
         this.div = document.createElement("div");
         this.div.className = "item";
         this.div.style.background = this.color;
-        this.div.style.height = "25%";
-        this.div.style.width = "15%";
-        this.div.style.margin = "auto";
         parentElement.appendChild(this.div);
     }
 }
 
 for (var i = 0; i<12 ; i++){
-    cardObj.push( new Card(colours[Math.floor(Math.random()*3)]));
+    cardObj.push( new Card(colours[Math.floor(Math.random()*6)]));
     cardObj[i].add();
 }
 
 setTimeout(() => {
     cardObj.forEach(card => {
-        card.div.style.background = "black";
+       blackOut(card);
     });
 }, 5000);
+
+function blackOut(card){
+    card.div.style.background = "black";
+}
 
 cardObj.forEach(card => {
     card.div.addEventListener("click",(event) => {
         event.target.style.background = card.color;
+        if (cardClicked){
+            first = card;
+            cardClicked = false;
+            if (first.div.style.background == second.div.style.background &(first.flipped == false & second.flipped == false)){
+                match(first,second);
+                console.log("yes");
+            }
+            else {
+                blackOut(first);
+                blackOut(second);
+            }
+        }
+        else {
+            second = card;
+            cardClicked = true;
+        }
 })});
 
+function match(first,second){
+    first.flipped = true;
+    second.flipped = true;
+}
