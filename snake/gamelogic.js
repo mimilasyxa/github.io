76da1be718c2083f3e265eed2ea7fe569ff2027d
mimilasyxa <50004735+mimilasyxa@users.noreply.github.com>
2019-07-15 17:27:38 +0700

    snaaake

---
+++ snake/gamelogic.js
@@ -0,0 +1,54 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var x = 100;
+var y = 100;
+var snake_moving;
+
+canvas.height = document.documentElement.clientHeight;
+canvas.width = document.documentElement.clientWidth;
+setInterval(function() {
+    window.requestAnimationFrame(drawing())
+},1000/15);
+
+
+
+function drawing(){
+    switch (snake_moving){
+        case ("up"):
+            y-=10;
+            break;
+        case ("right"):
+            x+=10;
+            break;
+        case ("down"):
+            y+=10;
+            break;
+        case ("left"):
+            x-=10;
+            break;
+    }
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.beginPath();
+    ctx.rect(x,y,10,10);
+    ctx.stroke();
+}
+
+document.addEventListener( "keydown" , (e)=> {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+        case 38:
+            snake_moving = "up";
+            break;
+        case 39:
+            snake_moving = "right";
+            break;
+        case 40:
+            snake_moving = "down";
+            break;
+        case 37:
+            snake_moving = "left";
+            break;
+        default:
+            break;
+    }
+})
