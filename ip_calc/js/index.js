66d8422b9fc39d5e920728994858c21337cb14d0
mimilasyxa <50004735+mimilasyxa@users.noreply.github.com>
2020-11-24 17:48:32 +0700

    upd - beta version up_calculator

--- ip_calc/js/index.js
+++ ip_calc/js/index.js
@@ -1,54 +1,75 @@
 let ip_input = document.querySelector("#ip"),
     mask_input = document.querySelector("#mask"),
     btn = document.querySelector("button"),
-    first_ip = [1],
-    last_ip = [],
     ip_bin_array = [],
     mask_bin_array = [],
     ip = document.querySelector(".ip_"),
     mask = document.querySelector(".mask_"),
-    node = document.querySelector(".node");
-    first = document.querySelector(".first");
-    last = document.querySelector(".last");
-    network = [];
-    network_bin = [];
+    node = document.querySelector(".node"),
+    first = document.querySelector(".first"),
+    last = document.querySelector(".last"),
+    broadcast = document.querySelector(".broadcast"),
+    network = [],
+    network_bin = [],
     network_number = 0;
 
-
+function checkInputs(mask_array,ip_array){
+    for (var i = 0; i < mask_array.length - 1; i++){
+        if (mask_array[i] < mask_array[i+1] || mask_array.length < 4 || ((isNaN(mask_array[i]) == true))){
+            return false;
+        }
+        else return true;
+    }
+    for (var i = 0; i < ip_array.length; i++){
+        if ((isNaN(mask_array[i]) == true || ip_array.length < 4)){
+            return false;
+        }
+        else return true;
+    }
+}
 
 btn.addEventListener("click", ()=>{
     ip_array = ip_input.value.split(".");
     mask_array = mask_input.value.split(".");
-    last_ip[network_number] = 255 - Number(mask_array[3]);
-    processInfo();
-    getNetwork(ip_array, mask_array);
-    getFirstnLast();
-})
-
-function dec2bin(dec){
-    if (Array.isArray(dec) == false){
-        return (dec >>> 0).toString(2);
+    if (checkInputs(mask_array, ip_array)){
+        processInfo();
+        getNetwork(ip_array, mask_array);
+        getFirstnLast();
+        getBroadcast();
     }
     else {
-        result = "";
-        for (var i = 0; i< 4; i++){
-            result+= (dec[i] >>> 0).toString(2) + ".";
-        }
-        return result.substring(0, result.length - 1);
+        ip.innerHTML = "Неправильный ввод";
     }
-}
+
+})
 
 function getFirstnLast(){
-    var ip_bin_array = [];
+    var first_ip= [];
+    var last_ip = [];
     for (var i= 0; i < 3; i++){
-        ip_bin_array[i] = Number(ip_array[i]);
+        first_ip[i] = (Number(ip_array[i]) & Number(mask_array[i]));
     }
-    ip_bin_array[3] = first_ip[network_number];
-    first.innerHTML = "первый адрес в сети =" + res2string(ip_bin_array) + " в двоином виде = " + dec2bin(ip_bin_array);
-    ip_bin_array[3] = last_ip[network_number];
-    last.innerHTML = "последний адрес в сети =" + res2string(ip_bin_array) + " в двоином виде = " + dec2bin(ip_bin_array) ;
-
-
+    first_ip[3] = (Number(ip_array[i]) & Number(mask_array[i])) + 1;
+    for (var i= 0; i < 4; i++){
+        if ((Number(ip_array[i]) & Number(mask_array[i])) == 0){
+            if (i == 3){
+                last_ip[i] = 254 -  Number(mask_array[i]); 
+            }
+            else {
+                last_ip[i] = 255 -  Number(mask_array[i]); 
+            }
+        }
+        else {
+            last_ip[i] = Number(ip_array[i]) & Number(mask_array[i]);
+        }
+    }
+    if (mask_array[3] == 255){
+        first.innerHTML = "В заданной сети нет адресов для рабочих хостов. ";
+    }
+    else {
+        first.innerHTML = "первый адрес в сети =" + res2string(first_ip) + " в двоином виде = " + dec2bin(first_ip);
+        last.innerHTML = "последний адрес в сети =" + res2string(last_ip) + " в двоином виде = " + dec2bin(last_ip);
+    }
 }
 
 function processInfo(){
@@ -63,16 +84,41 @@ function processInfo(){
 }
 
 function getNetwork(ip, mask){
-    for (var i = 0; i<3 ; i++){
+    for (var i = 0; i<4 ; i++){
         difference = (Number(ip[i]) & Number(mask[i]))
         network.push(difference);
         network_bin.push(dec2bin(difference));
     }
-    network[3] = Number(first_ip[network_number]) - 1;
-    network_bin[3] = dec2bin(Number(first_ip[network_number]) - 1);
+
     node.innerHTML = "адрес сети = " + res2string(network) + " в двоичном виде " + res2string(network_bin);
     network = [];
     network_bin= [];
+}
+
+function getBroadcast(){
+    var broadcast_array = [];
+    for (var i= 0; i < 4; i++){
+        if ((Number(ip_array[i]) & Number(mask_array[i])) == 0){
+                broadcast_array[i] = 255 -  Number(mask_array[i]); 
+        }
+        else {
+            broadcast_array[i] = Number(ip_array[i]) & Number(mask_array[i]);
+        }
+    }
+    broadcast.innerHTML = "широковещательный адрес = " + res2string(broadcast_array) + " в двоичном виде " + dec2bin(broadcast_array);
+}
+
+function dec2bin(dec){
+    if (Array.isArray(dec) == false){
+        return (dec >>> 0).toString(2);
+    }
+    else {
+        result = "";
+        for (var i = 0; i< 4; i++){
+            result+= (dec[i] >>> 0).toString(2) + ".";
+        }
+        return result.substring(0, result.length - 1);
+    }
 }
 
 function res2string(array){
