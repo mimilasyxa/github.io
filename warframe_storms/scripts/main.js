cbbc097b78262589920daa429f865111eb928b3a
aleksandr samorukov <mimilasyxa@gmail.com>
2022-11-27 10:08:57 +0700

    feat(Warframe-storm): вывод шмтормов рейлджека

---
+++ warframe_storms/scripts/main.js
@@ -0,0 +1,66 @@
+const TIERS = [
+    'Лит',
+    'Мезо',
+    'Нео',
+    'Акси',
+    'Реквием'
+];
+
+const MISSION_INFO = [
+    'tier', 'missionType', 'enemy', 'node', 'eta'
+]
+
+var xhr = new XMLHttpRequest()
+xhr.open(
+    'GET',
+    'https://api.warframestat.us/PC/fissures',
+    true
+)
+
+xhr.send()
+
+setTimeout(function () {
+    let $result = JSON.parse(xhr.responseText);
+    for (var i = 0; i < TIERS.length; i++) {
+        for (var k = 0; k < $result.length; k++) {
+            if ($result[k].isStorm === true) {
+                if ($fissure = filterResponse($result[k], TIERS[i])){
+                    createDIV(filterResponse($result[k], TIERS[i]), items[0]);
+                }
+            }
+        }
+    }
+
+
+}, 1000)
+
+let items = Array.from(document.querySelectorAll(".package"));
+
+function createDIV(value, parent) {
+    let div = document.createElement("div");
+    div.classList.add("fissure");
+    parent.appendChild(div);
+    createFissure(div, value);
+}
+
+function createFissure(parent, value) {
+    for (let i = 0; i < MISSION_INFO.length; i++) {
+        div = document.createElement('div')
+        if (MISSION_INFO[i] == 'node'){
+            div.classList.add('title-location');
+        } else {
+            div.classList.add('title-item');
+        }
+        if (value.node == 'Рудники Ню-Гуа (Нептун)'){
+            div.classList.add('golden');
+        }
+        div.innerHTML = value[MISSION_INFO[i]];
+        parent.appendChild(div);
+    }
+}
+
+function filterResponse(result, tier){
+    if (result.tier === tier){
+        return result
+    }
+}
