b82727935ec0c97c6a12ebc1300cd02fbaeadcc5
aleksandr samorukov <mimilasyxa@gmail.com>
2022-11-27 11:43:12 +0700

    fix(Warframe-storm): автообновление

--- warframe_storms/scripts/main.js
+++ warframe_storms/scripts/main.js
@@ -10,31 +10,34 @@ const MISSION_INFO = [
     'tier', 'missionType', 'enemy', 'node', 'eta'
 ]
 
+let items = Array.from(document.querySelectorAll(".package"));
+
+
 var xhr = new XMLHttpRequest()
-xhr.open(
-    'GET',
-    'https://api.warframestat.us/PC/fissures',
-    true
-)
+setInterval(()=> {
+    xhr.open(
+        'GET',
+        'https://api.warframestat.us/PC/fissures',
+        true
+    )
+    xhr.send()
+}, 8000)
 
-xhr.send()
 
-setTimeout(function () {
+
+setInterval(function () {
+    clearPage();
     let $result = JSON.parse(xhr.responseText);
     for (var i = 0; i < TIERS.length; i++) {
         for (var k = 0; k < $result.length; k++) {
             if ($result[k].isStorm === true) {
-                if ($fissure = filterResponse($result[k], TIERS[i])){
+                if ($fissure = filterResponse($result[k], TIERS[i])) {
                     createDIV(filterResponse($result[k], TIERS[i]), items[0]);
                 }
             }
         }
     }
-
-
-}, 1000)
-
-let items = Array.from(document.querySelectorAll(".package"));
+}, 10000)
 
 function createDIV(value, parent) {
     let div = document.createElement("div");
@@ -43,15 +46,23 @@ function createDIV(value, parent) {
     createFissure(div, value);
 }
 
+function clearPage() {
+    let $fissures = document.querySelectorAll('.fissure')
+
+    $fissures.forEach($fissure => {
+        $fissure.remove();
+    })
+}
+
 function createFissure(parent, value) {
     for (let i = 0; i < MISSION_INFO.length; i++) {
         div = document.createElement('div')
-        if (MISSION_INFO[i] == 'node'){
+        if (MISSION_INFO[i] == 'node') {
             div.classList.add('title-location');
         } else {
             div.classList.add('title-item');
         }
-        if (value.node == 'Рудники Ню-Гуа (Нептун)'){
+        if (value.node == 'Рудники Ню-Гуа (Нептун)') {
             div.classList.add('golden');
         }
         div.innerHTML = value[MISSION_INFO[i]];
@@ -59,8 +70,8 @@ function createFissure(parent, value) {
     }
 }
 
-function filterResponse(result, tier){
-    if (result.tier === tier){
+function filterResponse(result, tier) {
+    if (result.tier === tier) {
         return result
     }
 }
